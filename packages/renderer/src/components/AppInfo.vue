892605a390e8b498ffa7e64a4086f6634a3d8b54
luckypeppa <871565198@qq.com>
2022-12-08 15:15:28 +0800

    Ajust EncrptionHome.

--- packages/renderer/src/components/AppInfo.vue
+++ packages/renderer/src/components/AppInfo.vue
@@ -1,30 +1,22 @@
 <template>
   <div>
     <div class="info">{{ info }}</div>
-    <div class="actions">
-      <BaseButton type="warning">取消</BaseButton>
-      <BaseButton type="success">打开文件位置</BaseButton>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue';
-import BaseButton from './BaseButton.vue';
 
-const info = ref('已打开嘀嘀嘀.txt');
+defineProps<{
+  info: string;
+}>();
 </script>
 
 <style scoped lang="scss">
 .info {
-    color: var(--text-main);
-    font-size: 1.2rem;
-    font-weight: 600;
+  color: var(--text-main);
+  font-size: 1.2rem;
+  font-weight: 600;
 }
 
-.actions {
-    margin-top: 0.8rem;
-    display: grid;
-    place-items: center;
-}
+
 </style>
--- packages/renderer/src/components/EncryptionHome.vue
+++ packages/renderer/src/components/EncryptionHome.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="container">
-    <AppLoading :loading="loading">打 开</AppLoading>
-    <AppInfo />
+    <AppLoading
+      :loading="loading"
+      @click="getInputFile"
+    >
+      {{ mainButtonText }}
+    </AppLoading>
+    <AppInfo :info="info" />
+    <div class="actions">
+      <BaseButton
+        v-if="formState.inputPath"
+        type="warning"
+        @click="clearInputPath"
+      >
+        取消
+      </BaseButton>
+      <BaseButton type="success">打开文件位置</BaseButton>
+    </div>
     <div class="password">
       <BaseInput
-        v-model="password"
+        v-model="formState.password"
         type="password"
         placeholder="输入密码"
       ></BaseInput>
     </div>
     <div class="password">
       <BaseInput
-        v-model="file"
+        v-model="formState.outputPath"
         type="dir"
         placeholder="选择输出目录"
       ></BaseInput>
@@ -20,25 +35,40 @@
 </template>
 
 <script setup lang="ts">
-// import {versions, selectFile, invokeEncrypt} from '#preload';
-import {ref} from 'vue';
+import {selectFile} from '#preload';
+import {ref, reactive, computed} from 'vue';
 import AppInfo from './AppInfo.vue';
 // import type {Event} from 'electron';
 import AppLoading from './AppLoading.vue';
 import BaseInput from './BaseInput.vue';
+import BaseButton from './BaseButton.vue';
 
 // const inputPath = ref('');
 // const outputPath = ref('');
 // const msg = ref('');
 const loading = ref(false);
-const password = ref('');
-const file = ref('');
+const formState = reactive({
+  inputPath: '',
+  password: '',
+  outputPath: '',
+});
+const mainButtonText = computed(() => {
+  return formState.inputPath ? '加密' : '打开';
+});
+const info = ref('');
 
-// const getInputFile = (type: 'file' | 'dir') => {
-//   selectFile(type, (event: Event, filePath: string) => {
-//     inputPath.value = filePath;
-//   });
-// };
+// input file
+const getInputFile = (type: 'file' | 'dir') => {
+  selectFile(type, (event: Event, filePath: string) => {
+    formState.inputPath = filePath;
+    info.value = '已选择' + filePath;
+  });
+};
+
+const clearInputPath = () => {
+  formState.inputPath = '';
+  info.value = '选择已取消';
+};
 
 // const getOutputPath = () => {
 //   selectFile('dir', (event: Event, filePath: string) => {
@@ -67,5 +97,12 @@ const file = ref('');
 
 .password {
   margin-top: 1rem;
+}
+.actions {
+  margin-top: 0.8rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
 }
 </style>
